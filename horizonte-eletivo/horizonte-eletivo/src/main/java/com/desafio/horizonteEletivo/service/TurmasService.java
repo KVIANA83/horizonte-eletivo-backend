72dbac3d29499e08397dbcaaf6963c39300e7e31
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-03-02 15:27:57 -0300

    Atualizar o TurmasService.java
    update no código

--- horizonte-eletivo/horizonte-eletivo/src/main/java/com/desafio/horizonteEletivo/service/TurmasService.java
+++ horizonte-eletivo/horizonte-eletivo/src/main/java/com/desafio/horizonteEletivo/service/TurmasService.java
@@ -1,5 +1,44 @@
 package com.desafio.horizonteEletivo.service;
 
+import com.desafio.horizonteEletivo.dto.TurmasDTO;
+import com.desafio.horizonteEletivo.model.Turmas;
+import com.desafio.horizonteEletivo.repository.TurmasRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
 public class TurmasService {
     
+    @Autowired
+    private TurmasRepository turmasRepository;
+    
+    public TurmasService(TurmasRepository turmasRepository) {
+        this.turmasRepository = turmasRepository;
+    }
+
+    public List<TurmasDTO> listarTodas() {
+        List<Turmas> turmas = turmasRepository.findAll();
+        return turmas.stream()
+                .map(this::converterParaDTO)
+                .collect(Collectors.toList());
+    }
+
+    public TurmasDTO pegarTurmaPeloId(long id) {
+        Optional<Turmas> turmaOptional = turmasRepository.findById(id);
+        return turmaOptional.map(this::converterParaDTO).orElse(null);
+    }
+    
+    private TurmasDTO converterParaDTO(Turmas turma) {
+        return TurmasDTO.builder()
+                .idTurmas(turma.getIdTurmas())
+                .nomeTurma(turma.getNomeTurma())
+                .curso(turma.getCurso())
+                .modalidade(turma.getModalidade())
+                .quantVagas(turma.getQuantVagas())
+                .build();
+    }
 }
