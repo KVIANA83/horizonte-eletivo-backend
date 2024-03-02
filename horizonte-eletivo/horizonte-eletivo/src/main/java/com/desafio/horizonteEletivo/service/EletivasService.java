401e69b46498e0b1533cb0dbd9bd300bf25d4e6a
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-03-02 15:27:29 -0300

    Atualizar o EletivasService.java
    update no código

--- horizonte-eletivo/horizonte-eletivo/src/main/java/com/desafio/horizonteEletivo/service/EletivasService.java
+++ horizonte-eletivo/horizonte-eletivo/src/main/java/com/desafio/horizonteEletivo/service/EletivasService.java
@@ -1,5 +1,44 @@
 package com.desafio.horizonteEletivo.service;
 
-public class EletivasService {
+import com.desafio.horizonteEletivo.dto.EletivaDTO;
+import com.desafio.horizonteEletivo.model.Eletiva;
+import com.desafio.horizonteEletivo.repository.EletivaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EletivaService {
+    
+    @Autowired
+    private EletivaRepository eletivaRepository;
+    
+    public EletivaService(EletivaRepository eletivaRepository) {
+        this.eletivaRepository = eletivaRepository;
+    }
+
+    public List<EletivaDTO> listarTodas() {
+        List<Eletiva> eletivas = eletivaRepository.findAll();
+        return eletivas.stream()
+                .map(this::converterParaDTO)
+                .collect(Collectors.toList());
+    }
+    
+    public EletivaDTO pegarEletivaPeloId(long id) {
+        Optional<Eletiva> eletivaOptional = eletivaRepository.findById(id);
+        return eletivaOptional.map(this::converterParaDTO).orElse(null);
+    }
+    
+    private EletivaDTO converterParaDTO(Eletiva eletiva) {
+        return EletivaDTO.builder()
+                .idEletiva(eletiva.getIdEletiva())
+                .nomeProfessor(eletiva.getNomeProfessor())
+                .quantVagas(eletiva.getQuantVagas())
+                .nomeEletiva(eletiva.getNomeEletiva())
+                .build();
+    }
     
 }
